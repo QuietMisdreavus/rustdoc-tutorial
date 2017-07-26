a031550ebb791cca23521fbece5a064469d59c71
QuietMisdreavus <grey@quietmisdreavus.net>
2017-07-26 13:30:34 -0500

    update bonus crate

--- tutorial-bonus/src/lib.rs
+++ tutorial-bonus/src/lib.rs
@@ -1,1 +1,5 @@
 //! Bonus crate!
+//!
+//! Congratulations for finding the bonus crate! Like the main tutorial, this page actually
+//! represents a crate root, for a "separate library". You can click the "tutorial" entry in the
+//! crates listing in the sidebar to get back to the main page.
